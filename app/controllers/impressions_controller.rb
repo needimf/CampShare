4973791f80f1a4c4c77707d7d1f70f58a989363c
Nedim Filipovic <nedim.filipovic@gmail.com>
2017-10-30 15:26:21 -0700

    Created impressions controller actions/views, added count_total_impressions method to campground model, and added impressions data to campground show view.

--- app/controllers/impressions_controller.rb
+++ app/controllers/impressions_controller.rb
@@ -1,24 +1,46 @@
 class ImpressionsController < ApplicationController
   before_action :authorize
 
-  def new
+  def index
 
+  end
+
+  def new
+    @campground = Campground.find(params[:campground_id])
+    @impression = Impression.new
   end
 
   def create
-
+    @campground = Campground.find(params[:campground_id])
+    @impression = Impression.new(impresssions_params)
+    @impression.campground = @campground
+    @impression.user = current_user
+    if @impression.save
+      redirect_to campground_path(@impression.campground)
+    else
+      render :new
+    end
   end
 
   def edit
-
+    @impression = Impression.find(params[:id])
   end
 
   def update
+    @impression = Impression.find(params[:id])
 
+    if @impression.update_attributes(impresssions_params)
+      redirect_to campground_path(@impression.campground)
+    else
+      render :edit
+    end
   end
 
   def destroy
-
+    @impression = Impression.find(params[:id])
+    @campground = @impression.campground
+    @impression.destroy
+    redirect_to campground_path(@campground)
   end
 
 private
--- app/models/campground.rb
+++ app/models/campground.rb
@@ -4,4 +4,18 @@ class Campground < ApplicationRecord
   has_many :campground_images, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :impressions, dependent: :destroy
+
+  def count_impressions_totals
+    { clean_yes: self.impressions.where(clean: 1).count,
+      clean_no: self.impressions.where(clean: -1).count,
+      crowded_yes: self.impressions.where(crowded: 1).count,
+      crowded_no: self.impressions.where(crowded: -1).count,
+      shade_yes: self.impressions.where(shade: 1).count,
+      shade_no: self.impressions.where(shade: -1).count,
+      privacy_yes: self.impressions.where(privacy: 1).count,
+      privacy_no: self.impressions.where(privacy: -1).count,
+      fire_yes: self.impressions.where(fire: 1).count,
+      fire_no: self.impressions.where(fire: -1).count
+    }
+  end
 end
